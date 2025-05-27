41d494c1bc39ca744590d4bf8ab20211c2415014
Tenemakan <tenemakankone2004@gmail.com>
2025-05-27 03:27:20 +0000

    first commit6

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,23 +12,6 @@
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
 </head>
 <body>
-    <header>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="container">
-                <a class="navbar-brand" href="#">Plaintes</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav ms-auto">
-                        <li class="nav-item"><a class="nav-link active" href="#">Accueil</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#">À propos</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#">Contact</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    </header>
 
     <section class="banner py-5 position-relative text-white banner-enhanced">
         <div class="banner-overlay position-absolute top-0 start-0 w-100 h-100"></div>
